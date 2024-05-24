7eb05e7beb08010dac0598f76949dd75d5ef4913
VirajM <demunim@insharptechnologies.com>
2024-05-24 13:49:45 +0530

    Left side bar completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,30 @@
+import MobileNav from "@/components/MobileNav";
+import SideBar from "@/components/SideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: 'Viraj', lastName: 'Madhushan' }
+
   return (
-    <main>
-        SIDE BAR
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="mobile-menu" />
+
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+
         {children}
+      </div>
     </main>
   );
 }
---
+++ components/SideBar.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const SideBar = ({ user }: SiderbarProps) => {
+
+    const pathname = usePathname();
+
+    return (
+        <section className='sidebar'>
+            <nav>
+                {/* Home Link */}
+                <div className='mb-12 cursor-pointer items-center gap-2'>
+                    <Link href="/" >
+                        <Image
+                            src="/icons/logo.svg"
+                            width={34}
+                            height={34}
+                            alt='home-logo'
+                            className='size-[24px] max-xl:size-14'
+                        />
+                        <h1 className='sidebar-logo'>Horizon</h1>
+                    </Link>
+                </div>
+
+
+                {/* Dyanamic Side Bar Links */}
+                {sidebarLinks.map((item) => {
+                    const isActive = pathname === item.route || pathname.startsWith
+                        (`${item.route}/`)
+
+                    return (
+                        <div key={item.label}>
+                            <Link
+                                href={item.route}
+                                className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}>
+
+                                <div className='relative size-6'>
+                                    <Image
+                                        src={item.imgURL}
+                                        alt={item.label}
+                                        fill
+                                        className={cn({ 'brightness-[3] invert-0': isActive })}
+                                    />
+                                </div>
+
+                                <p className={cn('sidebar-label', { '!text-white': isActive })}>{item.label}</p>
+                                
+                            </Link>
+                        </div>
+                    );
+                })}
+
+                {/* User */}
+                User
+            </nav>
+
+            {/* footer */}
+        </section>
+    )
+}
+
+export default SideBar
